b28a1323eb3f94484495d3a586f0c9be794298a2
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2023-02-07 14:33:01 +0100

    fix choice and button

--- src/components/ShopComponent.vue
+++ src/components/ShopComponent.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="flex flex-col">
-		<div class="w-[80%] min-w-[20%] h-[80%] flex flex-col justify-center">
-			<button v-on:click="moveToPanier">Mon panier</button>
-			<select v-model="marchandSelected">
-				<option class="rounded-xl" v-for="marchand of marchands" v-bind:value="marchand" v-bind:key="marchand">
+		<div class="w-full  h-full flex flex-col space-y-4 justify-center">
+			<button class="rounded-xl mx-[40%] p-4 bg-[#ffc72c]" v-on:click="moveToPanier">Mon panier</button>
+			<select class="mx-[40%] rounded-xl" v-model="marchandSelected">
+				<option  v-for="marchand of marchands" v-bind:value="marchand" v-bind:key="marchand">
 				{{ marchand.nom }}
 				</option>
 			</select>
-			<button v-on:click="changeCard">choisir ce commercant</button>
+			<button class="rounded-xl p-4 bg-[#ffc72c] mx-[40%]" v-on:click="changeCard">choisir ce commercant</button>
 
 			<div v-for="produit in produits" :key="produit">
 				<ProduitComponent :produit="produit" :showAAP="true" @addToPanier="addToPanier"/>
